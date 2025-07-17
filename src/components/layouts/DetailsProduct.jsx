3d41b65b517aac4d6410deec0900d0def74c93e0
Lio3307 <aureliofs01@gmail.com>
2025-07-17 18:50:54 +0800

    fix style to grid

--- src/components/layouts/DetailsProduct.jsx
+++ src/components/layouts/DetailsProduct.jsx
@@ -17,12 +17,12 @@ const DetailsProduct = () => {
   return (
     <>
       <section className="text-gray-600 body-font overflow-hidden">
-        <div className="container px-5 mt-20 mx-auto">
+        <div className="px-5 mt-20 mx-auto">
           <div className="lg:w-4/5 mx-auto flex flex-wrap">
             <img
               alt="ecommerce"
-              className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded"
-              src="https://dummyimage.com/400x400"
+              className="lg:w-1/3 w-full lg:h-auto h-64 object-contain object-center rounded"
+              src={detailProduct.image}
             />
             <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
               <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">
@@ -30,11 +30,11 @@ const DetailsProduct = () => {
               </h1>
               <div className="flex mb-4"></div>
               <p className="leading-relaxed">{detailProduct.description}</p>
-              <div className="flex">
+              <div className="grid grid-cols-2">
                 <span className="title-font font-medium text-2xl text-gray-900">
                   ${detailProduct.price}
                 </span>
-                <button className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
+                <button className="grid ml-auto text-center text-white bg-indigo-500 border-0   focus:outline-none hover:bg-indigo-600 rounded">
                   Buy
                 </button>
               </div>
