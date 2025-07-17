7ac1a1ed19d4872e70320a96fe28dbb76e87e682
Lio3307 <aureliofs01@gmail.com>
2025-07-17 19:20:06 +0800

    change style, adding +cart button

--- src/components/layouts/DetailsProduct.jsx
+++ src/components/layouts/DetailsProduct.jsx
@@ -21,21 +21,24 @@ const DetailsProduct = () => {
           <div className="lg:w-4/5 mx-auto flex flex-wrap">
             <img
               alt="ecommerce"
-              className="lg:w-1/3 w-full lg:h-auto h-64 object-contain object-center rounded"
+              className="lg:w-1/3 w-full lg:h-100 h-64 object-contain object-center lg:mt-9 rounded"
               src={detailProduct.image}
             />
-            <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 lg:ml-[2rem] mt-[2rem] ">
+            <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 lg:ml-[4rem] mt-[2rem] ">
               <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">
                 {detailProduct.title}
               </h1>
               <div className="flex mb-4"></div>
               <p className="leading-relaxed">{detailProduct.description}</p>
-              <div className="grid mt-[2rem] grid-cols-2">
+              <div className="grid my-[2rem] grid-cols-3">
                 <span className="title-font font-medium text-2xl text-gray-900">
                   ${detailProduct.price}
                 </span>
-                <button className="grid ml-auto text-white cursor-pointer bg-indigo-500 border-0 py-1 px-6 focus:outline-none hover:bg-indigo-600 rounded">
+                <button className="grid ml-1 text-white cursor-pointer bg-indigo-500 border-0 py-1 px-6 focus:outline-none hover:bg-indigo-600 rounded">
                   Buy
+                </button>
+                <button className="grid ml-1 text-center text-white cursor-pointer bg-green-500 border-0 py-1 px-6 focus:outline-none hover:bg-green-600 rounded">
+                  + Cart
                 </button>
               </div>
             </div>
