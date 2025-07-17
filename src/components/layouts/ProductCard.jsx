37c8be1714ee015200bccbe076e11e9c466c903c
Lio3307 <aureliofs01@gmail.com>
2025-07-17 17:24:06 +0800

    fixing .map

--- src/components/layouts/ProductCard.jsx
+++ src/components/layouts/ProductCard.jsx
@@ -10,16 +10,16 @@ const ProductCard = () => {
 
   return (
     <>
-      {productList.map((product) => (
-        <section key={product.id} className="text-gray-600 body-font">
-          <div className="container px-5 py-24 mx-auto">
-            <div className="flex flex-wrap -m-4">
-              <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+      <section  className="text-gray-600 body-font">
+        <div className="container px-5 my-24 mx-">
+          <div className="flex flex-wrap gap-2">
+            {productList.map((product) => (
+              <div key={product.id} className="lg:w-1/4 md:w-1/2 p-1 rounded-2xl border-2 w-full">
                 <a className="block relative h-48 rounded overflow-hidden">
                   <img
                     alt="ecommerce"
-                    className="object-cover object-center w-full h-full block"
-                    src="https://picsum.photos/420/260"
+                    className="object-cover rounded-2xl object-center w-full h-full block"
+                    src={product.image}
                   />
                 </a>
                 <div className="mt-4">
@@ -32,10 +32,10 @@ const ProductCard = () => {
                   <p className="mt-1">{product.price}</p>
                 </div>
               </div>
-            </div>
+            ))}
           </div>
-        </section>
-      ))}
+        </div>
+      </section>
     </>
   );
 };
