35c47dd3e48e68764b5ceb5d926646390757efde
Lio3307 <aureliofs01@gmail.com>
2025-07-21 20:20:06 +0800

    adding AddProduct route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./components/pages/Home";
 import DetailsProduct from "./components/layouts/DetailsProduct";
 import CartProduct from "./components/pages/CartProduct";
+import AddProduct from "./components/pages/AddProducts";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/detail-product/:id" element={<DetailsProduct />} />
         <Route path="/cart-product" element={<CartProduct />} />
+        <Route path="/add-product" element={<AddProduct/>}/>
       </Routes>
     </>
   );
