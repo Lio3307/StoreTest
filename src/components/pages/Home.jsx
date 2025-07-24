dd825aeaab02f246f28e48e0e8c9fde6ac659ba2
Lio3307 <aureliofs01@gmail.com>
2025-07-24 13:57:26 +0800

    installing fuse.js for search feature

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,9 +1,15 @@
+import { useState } from "react";
 import ProductCard from "../layouts/ProductCard";
+import useProduct from "../../state/useProduct";
 
 const Home = () => {
+
   return (
     <>
+      <div className="container">
+
       <ProductCard />
+      </div>
     </>
   );
 };
